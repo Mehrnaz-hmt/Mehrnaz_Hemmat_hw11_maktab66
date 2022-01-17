bf27fc3db30f1d80b1d4308846c9dd9c3014de55
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-01-18 01:43:36 +0330

    some changes added to q-1

--- Question 1/q-1.js
+++ Question 1/q-1.js
@@ -1,23 +1,34 @@
 // Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-console.log(myNodelist)
-for (let i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+
+
+let myNodelist = document.getElementsByTagName("LI");
+// let iTags = document.querySelectorAll("body>ul>li not:(.left-items)");
+// console.log("iTags")
+for (let i = 0; i <myNodelist.length-2; i++) {
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.style.fontSize = "xx-large";
   span.style.fontWeight = "lighter";
-  span.style.color = "grey"
+  span.style.color = "grey";
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
 
+
+
+
+
+
+
+
+
+
 // Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
+let close = document.getElementsByClassName("close");
+for (let i = 0; i < close.length; i++) {
   close[i].onclick = function() {
-    var div = this.parentElement;
+    let div = this.parentElement;
     div.style.display = "none";
   }
 }
@@ -29,6 +40,8 @@ list.addEventListener('click', function(ev) {
     ev.target.classList.toggle('checked');
   }
 }, false);
+
+
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
