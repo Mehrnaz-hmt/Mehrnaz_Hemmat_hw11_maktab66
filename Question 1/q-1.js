374f83fc7b4eb5db16ff0220d9dbbbb1e15dade5
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-01-19 23:33:32 +0330

    some styles on q-1 added

--- Question 1/q-1.js
+++ Question 1/q-1.js
@@ -1,9 +1,12 @@
+let element = document.createElement("li");
+    element.classList.add("todo");
+console.log(element)
+
+
+const clearCompleted = document.querySelector(".clear-completed")
+
 // Create a "close" button and append it to each list item
-
-
 let myNodelist = document.getElementsByTagName("LI");
-// let iTags = document.querySelectorAll("body>ul>li not:(.left-items)");
-// console.log("iTags")
 for (let i = 0; i <myNodelist.length-2; i++) {
   let span = document.createElement("SPAN");
   let txt = document.createTextNode("\u00D7");
@@ -14,15 +17,6 @@ for (let i = 0; i <myNodelist.length-2; i++) {
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
-
-
-
-
-
-
-
-
-
 
 // Click on a close button to hide the current list item
 let close = document.getElementsByClassName("close");
@@ -45,27 +39,66 @@ list.addEventListener('click', function(ev) {
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
+  let li = document.createElement("li");
+  let inputValue = document.getElementById("myInput").value;
+  let t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    listArray = []; //Me
+    // alert("You must write something!"); //program
   } else {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("myInput").value = "";
 
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("\u00D7");
+  console.log(txt)
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function() {
-      var div = this.parentElement;
+      let div = this.parentElement;
       div.style.display = "none";
     }
   }
 }
+
+//show pending items 
+function showPendingItems () {
+  let getLocalStorageData = localStorage.getItem("New Todo");
+  if(getLocalStorageData == null){
+    listArray = [];
+  }else{
+    listArray = JSON.parse(getLocalStorageData); 
+  }
+  const pendingItemsNumb = document.querySelector(".pending-items");
+
+}
+//===============================================
+// let theme = document.querySelector(".moon"); //icon
+// let body = document.querySelector("body");
+// let ind = false;
+// theme.addEventListener("click",function(){
+//     if(ind == true){
+//         // document.body.classList.remove("light");
+//        body.style.background = "black"
+//         ind = false;
+//         theme.src ="./images/bg-desktop-dark.jpg";
+        
+//         header.style.background = "url(./images/icons/sun-3-16.png) no-repeat center center width:15px/ cover"
+//     }
+//     else{
+//         document.body.classList.remove("dark");
+//         document.body.classList.add("light");
+//         ind = true;
+//         theme.src ="./images/icon-moon.svg"
+//         header.style.background = "url(./images/bg-desktop-light.jpg) no-repeat center center / cover"
+
+//     }
+// });
+// var element = document.getElementById("myUL");
+// var numberOfChildren = element.getElementsByTagName('li').length
+// console.log(numberOfChildren)
